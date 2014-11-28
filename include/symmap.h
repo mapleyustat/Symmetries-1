120df9ae7547849be8e10fe9d4694788c34d2a25
Nils Wentzell <nils.wentzell@gmail.com>
2014-11-28 18:52:10 +0100

    added mirror symmetries

--- include/symmap.h
+++ include/symmap.h
@@ -25,15 +25,21 @@ operation rot_k(index_t& ind);		///< Rotate all momenta by 90 degrees - IMPLEMEN
 operation compl_conj(index_t& ind);	///< Complex conjugation
 operation exch_out(index_t& ind);	///< Exchange outgoing lines
 operation time_rev(index_t& ind);	///< Time reversal symmetry
-
+operation mirror_vert(index_t& ind);	///< Mirror all momenta vertically
+operation mirror_diag(index_t& ind);	///< Mirror all momenta diagonally
 
 // Helper functions
 void freq_sign_change(int& ind);	///< Change sign of signle frequency
 void mom_sign_change(int& ind);		///< Change sign of single momentum
+void mirror_mom_vert(int& ind);		///< Mirror momentum at vertical axis
+void mirror_mom_diag(int& ind);		///< Mirror momentum at diagonal (bottom left to top right) axis
 void swap(int& a, int& b);		///< Swap two numbers
 
-const int rot_k_ind_arr[8] = {0, 2,3,4,1, 7, 6, 5}; ///< Array that specifies how to rotate single momentum index
-const int sign_change_k_ind_arr[8] = {0, 3, 4, 1, 2, 5, 6, 7}; ///< Array that specifies how to change sign of single momentum index ( corresponds to 2 rotations)
+const int rot_k_ind_arr[8] = {0, 2, 3, 4, 1, 7, 6, 5}; ///< Array that specifies how to rotate single momentum index
+const int sign_change_k_ind_arr[8] = {0, 3, 4, 1, 2, 5, 6, 7}; ///< Array that specifies how to change sign of single momentum index ( corresponds to 2 rotations )
+const int mirror_mom_vert_arr[8] = { 0, 4, 3, 2, 1, 5, 6, 7 }; ///< Array that specifies how to mirror single momentum index at vertical axis
+const int mirror_mom_diag_arr[8] = { 0, 1, 4, 3, 2, 7, 6, 5 }; ///< Array that specifies how to mirror single momentum index at diagonal ( bottom left to top right ) axis
+
 /**
  *	Calculate the sum of two momenta
  */
--- src/test.cpp
+++ src/test.cpp
@@ -24,6 +24,8 @@ int main (int argc, char * argv[])
    symm_func_list.push_back(rot_k);
    symm_func_list.push_back(compl_conj);
    symm_func_list.push_back(time_rev);
+   symm_func_list.push_back(mirror_vert);
+   symm_func_list.push_back(mirror_diag);
 
    // Initialize vertex tensor
    shared_ptr<vertex_tensor> vertex(new vertex_tensor(FREQ_COUNT_VERT, PATCH_COUNT, QN_COUNT));
@@ -32,27 +34,28 @@ int main (int argc, char * argv[])
    vector<index_t> ind_cpl_list;
 
 
-   for(int w1_in = 0; w1_in < FREQ_COUNT_VERT; ++w1_in)
-      for(int w2_in = 0; w2_in < FREQ_COUNT_VERT; ++w2_in)
-	 for(int w1_out = 0; w1_out < FREQ_COUNT_VERT; ++w1_out)
-	    for(int k1_in = 0; k1_in < PATCH_COUNT; ++k1_in)
-	       for(int k2_in = 0; k2_in < PATCH_COUNT; ++k2_in)
-		  for(int k1_out = 0; k1_out < PATCH_COUNT; ++k1_out)
-		     for(int s1_in = 0; s1_in < QN_COUNT; ++s1_in)
-			for(int s2_in = 0; s2_in < QN_COUNT; ++s2_in)
-			   for(int s1_out = 0; s1_out < QN_COUNT; ++s1_out)
-			      for(int s2_out = 0; s2_out < QN_COUNT; ++s2_out)
-			      {
-				 index_t ind(w1_in, w2_in, w1_out, k1_in, k2_in, k1_out, s1_in, s2_in, s1_out, s2_out);
-				 if ( (*vertex)(ind).ind == -1 )  					// if tensor object not yet related to any other
+   for(int imre  = 0; imre < 2; ++imre)
+      for(int w1_in = 0; w1_in < FREQ_COUNT_VERT; ++w1_in)
+	 for(int w2_in = 0; w2_in < FREQ_COUNT_VERT; ++w2_in)
+	    for(int w1_out = 0; w1_out < FREQ_COUNT_VERT; ++w1_out)
+	       for(int k1_in = 0; k1_in < PATCH_COUNT; ++k1_in)
+		  for(int k2_in = 0; k2_in < PATCH_COUNT; ++k2_in)
+		     for(int k1_out = 0; k1_out < PATCH_COUNT; ++k1_out)
+			for(int s1_in = 0; s1_in < QN_COUNT; ++s1_in)
+			   for(int s2_in = 0; s2_in < QN_COUNT; ++s2_in)
+			      for(int s1_out = 0; s1_out < QN_COUNT; ++s1_out)
+				 for(int s2_out = 0; s2_out < QN_COUNT; ++s2_out)
 				 {
-				    ind_cpl_list.push_back(ind);					// push standard representative into ind_cpl_list
-				    int ind_cpl_list_pos =  ind_cpl_list.size() - 1 ; 			// position in ind_cpl_list is size - 1
-				    (*vertex)(ind) = ind_cpl_t( ind_cpl_list_pos ); 			// save position with trivial operations
-				    operation track_op(false,false);
-				    iterate(ind, track_op, *vertex, symm_func_list, ind_cpl_list_pos ); 		// start iterating on index 
+				    index_t ind(w1_in, w2_in, w1_out, k1_in, k2_in, k1_out, s1_in, s2_in, s1_out, s2_out);
+				    if ( (*vertex)(ind).ind == -1 )  					// if tensor object not yet related to any other
+				    {
+				       ind_cpl_list.push_back(ind);					// push standard representative into ind_cpl_list
+				       int ind_cpl_list_pos =  ind_cpl_list.size() - 1 ; 		// position in ind_cpl_list is size - 1
+				       (*vertex)(ind) = ind_cpl_t( ind_cpl_list_pos ); 			// save position with trivial operations
+				       operation track_op(false,false);
+				       iterate(ind, track_op, *vertex, symm_func_list, ind_cpl_list_pos ); 		// start iterating on index 
+				    }
 				 }
-			      }
 
    std::cout << " Number of tensor indices : " << TENSOR_IND_COUNT << endl;
    std::cout << " Number of independent couplings : " << ind_cpl_list.size() << endl;
